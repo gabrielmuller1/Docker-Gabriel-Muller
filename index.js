16d16fe12ae3cdd33083d6899bf4bbc9da2aa0d5
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-06-03 15:06:12 -0300

    Final-config-docker

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = 3001;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({message: "Hello, Gabriel!"});
-})
+app.use(express.json());
+
+app.post("/", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  response.json({
+    user: {
+      receiver: {
+        name: "Gabriel Muller",
+        idade: "19 anos",
+        sexo: "Masculino",
+        ano_nasc: "2002",
+      },
+      sender: {
+        name: "Ricardo",
+        idade: "XX anos",
+        sexo: "Masculino",
+        ano_nasc: "YYYY",
+      },
+    },
+  });
+});
 
 app.listen(PORT, HOST);
